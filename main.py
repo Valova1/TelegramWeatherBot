9bf739039567583c29d81eee0044011e794cef97
NikitaKolotushkin <nikitaplay1000@gmail.com>
2020-09-25 13:22:35 +0400

    Console functionality testing

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+import telebot
+from pyowm import OWM
+from tokens import *
+
+
+owm = OWM(pyowm_token)
+mgr = owm.weather_manager()
+observation = mgr.weather_at_place('Moscow')
+w = observation.weather
+print(w)
